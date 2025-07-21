c5f60c8f3453466e7d019dd306e3edb2265e61d8
Gexstunf <166758362+Gexstunf@users.noreply.github.com>
2025-07-21 11:59:45 -0300

    RoomLobbyConection and UI

--- Assets/InsideGame/Scripts/RoomLobbyManager.cs
+++ Assets/InsideGame/Scripts/RoomLobbyManager.cs
@@ -1,33 +1,47 @@
-using System.Collections.Generic;
-using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
 using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
 
-public class RoomLobbyUI : MonoBehaviourPunCallbacks
+public class RoomLobbyManager : MonoBehaviourPunCallbacks
 {
+    public TextMeshProUGUI roomNameText;
+    public GameObject playerListContainer;
     public GameObject playerEntryPrefab;
-    public Transform contentPanel;
+    public Button startGameButton;
+    public Button leaveRoomButton;
 
     private Dictionary<int, GameObject> playerEntries = new Dictionary<int, GameObject>();
 
     void Start()
     {
+        roomNameText.text = "Room: " + PhotonNetwork.CurrentRoom.Name;
+        leaveRoomButton.onClick.AddListener(LeaveRoom);
+
+        if (!PhotonNetwork.IsMasterClient)
+            startGameButton.gameObject.SetActive(false);
+        else
+            startGameButton.onClick.AddListener(StartGame);
+
         UpdatePlayerList();
     }
 
     void UpdatePlayerList()
     {
-        foreach (var entry in playerEntries.Values)
+        foreach (Transform child in playerListContainer.transform)
         {
-            Destroy(entry);
+            Destroy(child.gameObject);
         }
+
         playerEntries.Clear();
 
         foreach (Player player in PhotonNetwork.PlayerList)
         {
-            GameObject entry = Instantiate(playerEntryPrefab, contentPanel);
-            entry.transform.Find("PlayerNameText").GetComponent<TextMeshProUGUI>().text = player.NickName;
+            GameObject entry = Instantiate(playerEntryPrefab, playerListContainer.transform);
+            entry.GetComponent<TextMeshProUGUI>().text = player.NickName;
             playerEntries[player.ActorNumber] = entry;
         }
     }
@@ -40,5 +54,25 @@ public class RoomLobbyUI : MonoBehaviourPunCallbacks
     public override void OnPlayerLeftRoom(Player otherPlayer)
     {
         UpdatePlayerList();
+    }
+
+    public override void OnMasterClientSwitched(Player newMasterClient)
+    {
+        startGameButton.gameObject.SetActive(PhotonNetwork.IsMasterClient);
+    }
+
+    void LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+    }
+
+    public override void OnLeftRoom()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    void StartGame()
+    {
+        PhotonNetwork.LoadLevel("GameScene"); 
     }
 }
