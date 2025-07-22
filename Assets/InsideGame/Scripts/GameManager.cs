995740387f714ee8a7fdc765493bd671704551f8
Gexstunf <166758362+Gexstunf@users.noreply.github.com>
2025-07-22 14:32:00 -0300

    spawneo de players

---
+++ Assets/InsideGame/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+using Photon.Pun;
+using UnityEngine;
+
+public class GameManager : MonoBehaviourPunCallbacks
+{
+    public GameObject playerPrefab;
+    public Transform[] spawnPoints;
+
+    void Start()
+    {
+        if (PhotonNetwork.IsConnected)
+        {
+            int playerIndex = PhotonNetwork.LocalPlayer.ActorNumber - 1;
+
+            // Si hay menos jugadores que puntos, usar el punto correspondiente
+            // Si hay más, tomar aleatorio pero evitando superposición
+            Transform spawnPoint;
+            if (playerIndex < spawnPoints.Length)
+            {
+                spawnPoint = spawnPoints[playerIndex];
+            }
+            else
+            {
+                // Backup: tomar uno aleatorio no ocupado (mejorable)
+                spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
+            }
+
+            PhotonNetwork.Instantiate(playerPrefab.name, spawnPoint.position, spawnPoint.rotation);
+        }
+    }
+}
---
+++ Assets/InsideGame/Scripts/NetworkPlayer.cs
@@ -0,0 +1,25 @@
+﻿using Photon.Pun;
+using UnityEngine;
+
+public class NetworkPlayer : MonoBehaviourPun
+{
+    void Start()
+    {
+        if (!photonView.IsMine)
+        {
+            // Esto no es el jugador local → desactiva cámara o input
+            GetComponentInChildren<Camera>().enabled = false;
+        }
+    }
+
+    void Update()
+    {
+        if (photonView.IsMine)
+        {
+            // Movimiento simple para testear
+            float h = Input.GetAxis("Horizontal");
+            float v = Input.GetAxis("Vertical");
+            transform.Translate(new Vector3(h, 0, v) * Time.deltaTime * 5f);
+        }
+    }
+}
