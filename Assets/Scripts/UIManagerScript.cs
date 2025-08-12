e035675e34c9af7389cf3051a7e8339e9e8fbc4a
Gexstunf <166758362+Gexstunf@users.noreply.github.com>
2025-08-12 11:56:45 -0300

    Stamina and Health display code

--- Assets/Scripts/UIManagerScript.cs
+++ Assets/Scripts/UIManagerScript.cs
@@ -1,18 +1,46 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI; // Para usar Image
 
-namespace UI.Scripts.TestingUI {
-    public class UIManagerScript : MonoBehaviour
+public class UIManagerScript : MonoBehaviour
+{
+    [Header("Barra de Stamina")]
+    public Image staminaBar;
+    public float maxStamina;
+
+    [Header("Vida en Ticks")]
+    public Transform healthContainer;
+    public Sprite tickOn;
+    public Sprite tickOff;
+    public float maxHealth = 100f;
+    public float healthPerTick = 10f;
+
+    private List<Image> healthTicks = new List<Image>();
+    
+    public void DisplayStamina(float amount){
+        staminaBar.fillAmount = amount / maxStamina;
+    }
+
+    void Awake()
     {
-        public TextMeshProUGUI staminaText;
-        public TextMeshProUGUI healthText;
-
-        public void DisplayStamina(float amount) {
-            staminaText.text = amount.ToString();
+        foreach (Transform child in healthContainer)
+        {
+            Image img = child.GetComponent<Image>();
+            if (img != null)
+                healthTicks.Add(img);
         }
+    }
 
-        public void DisplayHealth(float amount) {
-            healthText.text = amount.ToString();
+    public void DisplayHealth(float amount)
+    {
+        int ticksOn = Mathf.CeilToInt(amount / healthPerTick);
+
+        for (int i = 0; i < healthTicks.Count; i++)
+        {
+            if (i < ticksOn)
+                healthTicks[i].sprite = tickOn;
+            else
+                healthTicks[i].sprite = tickOff;
         }
     }
 }
