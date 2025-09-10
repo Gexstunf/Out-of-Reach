f2c32c68004c5dbcf0182a7433afa879d0303b99
loucimf <145812460+loucimf@users.noreply.github.com>
2025-09-10 00:25:50 -0300

    FIXED  |   Imported new model with fixes

--- Assets/Characters/PlayerController/Scripts/CharAnimControllerScript.cs
+++ Assets/Characters/PlayerController/Scripts/CharAnimControllerScript.cs
@@ -1,15 +1,17 @@
 using System;
+using Characters.PlayerController.Scripts.Input;
 using UnityEngine;
 
 namespace Characters.PlayerController.Scripts {
     public class CharAnimControllerScript : MonoBehaviour
     {
-        private static readonly int Speed = Animator.StringToHash("Speed");
+        private static readonly int Move = Animator.StringToHash("Input");
         [SerializeField] private Animator animator;
         [SerializeField] private Rigidbody rigidbody;
+        [SerializeField] private PlayerInputScript playerInput;
 
         public void Update() {
-            animator.SetFloat(Speed, rigidbody.linearVelocity.magnitude);
+            animator.SetFloat(Move, playerInput.MoveInput.magnitude);
         }
     }
 }
