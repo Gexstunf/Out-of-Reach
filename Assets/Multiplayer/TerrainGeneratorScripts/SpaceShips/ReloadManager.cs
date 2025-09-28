982a300c99520996c0864451b2dccfeb57c356ba
MarguJ <margulesjoa@gmail.com>
2025-09-28 18:22:12 -0300

    Pre-Push before the caos

--- Assets/Multiplayer/TerrainGeneratorScripts/SpaceShips/ReloadManager.cs
+++ Assets/Multiplayer/TerrainGeneratorScripts/SpaceShips/ReloadManager.cs
@@ -1,11 +1,15 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+using System.Linq;
+using Object = UnityEngine.Object;
 
 namespace Multiplayer.TerrainGeneratorScripts.SpaceShips
 {
     public static class ReloadManager
     {
-        
+        public static Dictionary<string, GameObject> AllStructures = new();
         private static bool _reloading = false;
         
         [RuntimeInitializeOnLoadMethod]
@@ -17,9 +21,18 @@ namespace Multiplayer.TerrainGeneratorScripts.SpaceShips
             };
         }
 
-        public static void AddStructure()
+        public static void AddStructure(GameObject structure)
         {
-            
+            AllStructures.Add(structure.name, structure);
+        }
+
+        public static void RemoveStructure()
+        {
+            foreach (var structure in AllStructures.Skip(1))
+            {
+                Object.Destroy(structure.Value);
+                AllStructures.Remove(structure.Key);
+            }
         }
         
         public static void ReloadScene(string sceneName)
