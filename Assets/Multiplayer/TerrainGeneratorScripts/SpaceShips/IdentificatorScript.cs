07c6014f891d9b0c832a690f98c845d3d0cec92c
MarguJ <margulesjoa@gmail.com>
2025-10-13 14:08:59 -0300

    Added priority numbers (not functionality))

--- Assets/Multiplayer/TerrainGeneratorScripts/SpaceShips/IdentificatorScript.cs
+++ Assets/Multiplayer/TerrainGeneratorScripts/SpaceShips/IdentificatorScript.cs
@@ -5,12 +5,16 @@ namespace Multiplayer.TerrainGeneratorScripts.SpaceShips
 {
     public class IdentificatorScript : MonoBehaviour
     {
-        public static float PriorityNum;
+        public static float PriorityNum = 0;
+        public float uniquePriority;
         public string structureID;
         private void Awake()
         {
             if (string.IsNullOrEmpty(structureID))
                 structureID = Guid.NewGuid().ToString();
+            PriorityNum += 0.1f;
+            uniquePriority = PriorityNum;
+            Debug.Log(uniquePriority);
         }
 
         void Start()
