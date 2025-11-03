03a0815907b3a1131a9b112763acd13acbf32790
MarguJ <167230292+MarguJ@users.noreply.github.com>
2025-11-03 14:36:57 -0300

    Es dificil de creer
    No esta fucionando el collider la concha de tu puta madre loucim

--- Assets/Multiplayer/TerrainGeneratorScripts/Spaceships - 2/InfoScript.cs
+++ Assets/Multiplayer/TerrainGeneratorScripts/Spaceships - 2/InfoScript.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace Multiplayer.TerrainGeneratorScripts.Spaceships___2
@@ -6,27 +7,57 @@ namespace Multiplayer.TerrainGeneratorScripts.Spaceships___2
     {
         [SerializeField] BranchDivisionScript branchDivision;
         public static int RamaID = -1;
+        public LayerMask layerMask = LayerMask.GetMask("Terrain");
 
         private void Awake()
         {
             branchDivision = FindAnyObjectByType<BranchDivisionScript>();
         }
-
+        
         private void Start()
         {
-            if (gameObject.CompareTag("Intersection"))
+            Collider myCol = GetComponent<Collider>();
+            
+            if (gameObject.CompareTag("Intersection") && !IsOverlapping(layerMask, myCol.bounds.extents))
             {
                 branchDivision.CerrarRama(RamaID, false);
                 RamaID = branchDivision.CrearRama();
                 branchDivision.AgregarAHilo(RamaID, this);
             }
-            else if (gameObject.CompareTag("Room"))
+            else if (gameObject.CompareTag("Room") && !IsOverlapping(layerMask, myCol.bounds.extents))
             {
                 branchDivision.CerrarRama(RamaID, false);
                 RamaID = branchDivision.CrearRama();
                 branchDivision.AgregarAHilo(RamaID, this);
             }
-            Debug.Log(RamaID);
+            else if (IsOverlapping(layerMask, myCol.bounds.extents))
+            {
+                Debug.Log("Ta overlappeando");
+                branchDivision.CerrarRama(RamaID, true);
+            }
+        }
+        private bool IsOverlapping(LayerMask layer, Vector3 halfExtents)
+        {
+            Collider[] hits = Physics.OverlapBox(
+                transform.position,
+                halfExtents,
+                transform.rotation,
+                layer
+            );
+            
+            Debug.Log(hits.Length);
+            
+            foreach (var hit in hits)
+            {
+                if (hit.gameObject == gameObject)
+                    continue;
+                
+                if (hit.CompareTag("Room") || hit.CompareTag("Hallway") || hit.CompareTag("Intersection"))
+                {
+                    return true;
+                }
+            }
+            return hits.Length > 0;
         }
     }
 }
